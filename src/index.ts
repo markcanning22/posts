05383e2c5f562b79f0b3928ae75aefd8752c0d46
Mark Canning <markcanning22@gmail.com>
2023-04-04 23:00:22 +0100

    Increasing type safety

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import { randomBytes } from 'crypto';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -10,12 +10,12 @@ app.use(cors());
 
 const posts: {[index: string]: {}} = {};
 
-app.get('/posts', (req, res) => {
+app.get('/posts', (req: Request, res: Response): void => {
     res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
+app.post('/posts', async (req: Request, res: Response): Promise<void> => {
+    const id: string = randomBytes(4).toString('hex');
     const { title } = req.body;
 
     posts[id] = {
@@ -33,6 +33,6 @@ app.post('/posts', async (req, res) => {
     res.status(201).send(posts[id]);
 })
 
-app.listen(4000, () => {
+app.listen(4000, (): void => {
     console.log('Listening on 4000');
 });
